c47820846b279fefd3ba531b496935203a988c1b
Zuriel Mbonde <zuriel.mbonde@xandr.com>
2020-08-04 11:25:33 -0400

    Updates

---
+++ precache-manifest.591797597b289652159ee26473968cd8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94c74ce0cbfebbf1f305ed7805d7b179",
+    "url": "/prebid/index.html"
+  },
+  {
+    "revision": "15007a6d57d7944faba1",
+    "url": "/prebid/static/css/main.c2cadc0f.chunk.css"
+  },
+  {
+    "revision": "1dcfdf96c834bf364633",
+    "url": "/prebid/static/js/2.460eb0e5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/prebid/static/js/2.460eb0e5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "15007a6d57d7944faba1",
+    "url": "/prebid/static/js/main.9ca65cf8.chunk.js"
+  },
+  {
+    "revision": "d5de49fc84374c9adeff",
+    "url": "/prebid/static/js/runtime-main.66d56ad5.js"
+  }
+]);
